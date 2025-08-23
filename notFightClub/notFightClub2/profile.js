026c60bf29b2c32fb6344eeb16cd888a15d4d68e
Anna Khodosovskaya <hodosovskaaanna@gmail.com>
2025-08-24 02:57:49 +0300

    feat: add open modal window with img + save the img

--- notFightClub/notFightClub2/profile.js
+++ notFightClub/notFightClub2/profile.js
@@ -2,7 +2,11 @@ document.getElementById('name_character').innerHTML = localStorage.getItem('text
 
 const modal = document.getElementById('modal');
 const modalOpen = document.querySelector('.img_character');
-const btnClose = document.getElementById('closeBtn')
+const btnClose = document.getElementById('closeBtn');
+let wins = document.getElementById('wins');
+let loses = document.getElementById('loses');
+const characterModalImages = document.querySelectorAll('.characterModal');
+const characterImage = document.getElementById('current-character');
 
 function openModalWindow() {
     modal.style.display = 'flex';
@@ -11,6 +15,7 @@ function openModalWindow() {
 function closeModalWindow() {
     modal.style.display = 'none';
 }
+
 modalOpen.onclick = openModalWindow;
 
 window.onclick = function (e) {
@@ -18,45 +23,28 @@ window.onclick = function (e) {
         closeModalWindow();
     }
 
-    // Обработчик закрытия (кнопка)
-
     btnClose.addEventListener('click', function (e) {
         closeModalWindow();
-        e.stopPropagation(); // Предотвращаем всплытие события к modal
+        e.stopPropagation();
     });
-
 };
-
-
-
-//
-const characterModalImages = document.querySelectorAll('.characterModal');
-
-const characterImage = document.getElementById('current-character');
 
 characterModalImages.forEach(image => {
     image.addEventListener('click', () => {
         const selectedAvatarSrc = image.dataset.src;
-
         characterImage.src = selectedAvatarSrc;
-
         characterModalImages.forEach(img => img.classList.remove('selected'));
         image.classList.add('selected');
-
         localStorage.setItem('currentAvatar', selectedAvatarSrc);
     });
 });
 
-
-// При загрузке страницы восстановить сохраненный аватар
 window.addEventListener('load', () => {
     const savedAvatar = localStorage.getItem('currentAvatar');
     const characterModalImages = document.querySelectorAll('.characterModal');
-
     if (savedAvatar) {
         const characterImage = document.getElementById('current-character');
         characterImage.src = savedAvatar;
-
         characterModalImages.forEach(img => {
             if (img.dataset.src === savedAvatar) {
                 img.classList.add('selected');
@@ -65,5 +53,3 @@ window.addEventListener('load', () => {
     }
 });
 
-let wins = document.getElementById('wins');
-let loses = document.getElementById('loses');
