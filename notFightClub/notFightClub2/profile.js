cf767c4e87773ef5f969eb2d9977c9f2873cbfc4
Anna Khodosovskaya <hodosovskaaanna@gmail.com>
2025-08-24 02:52:27 +0300

    feat: add css properties to the page profile

--- notFightClub/notFightClub2/profile.js
+++ notFightClub/notFightClub2/profile.js
@@ -1,1 +1,69 @@
 document.getElementById('name_character').innerHTML = localStorage.getItem('textvalue');
+
+const modal = document.getElementById('modal');
+const modalOpen = document.querySelector('.img_character');
+const btnClose = document.getElementById('closeBtn')
+
+function openModalWindow() {
+    modal.style.display = 'flex';
+}
+
+function closeModalWindow() {
+    modal.style.display = 'none';
+}
+modalOpen.onclick = openModalWindow;
+
+window.onclick = function (e) {
+    if (e.target === modal) {
+        closeModalWindow();
+    }
+
+    // Обработчик закрытия (кнопка)
+
+    btnClose.addEventListener('click', function (e) {
+        closeModalWindow();
+        e.stopPropagation(); // Предотвращаем всплытие события к modal
+    });
+
+};
+
+
+
+//
+const characterModalImages = document.querySelectorAll('.characterModal');
+
+const characterImage = document.getElementById('current-character');
+
+characterModalImages.forEach(image => {
+    image.addEventListener('click', () => {
+        const selectedAvatarSrc = image.dataset.src;
+
+        characterImage.src = selectedAvatarSrc;
+
+        characterModalImages.forEach(img => img.classList.remove('selected'));
+        image.classList.add('selected');
+
+        localStorage.setItem('currentAvatar', selectedAvatarSrc);
+    });
+});
+
+
+// При загрузке страницы восстановить сохраненный аватар
+window.addEventListener('load', () => {
+    const savedAvatar = localStorage.getItem('currentAvatar');
+    const characterModalImages = document.querySelectorAll('.characterModal');
+
+    if (savedAvatar) {
+        const characterImage = document.getElementById('current-character');
+        characterImage.src = savedAvatar;
+
+        characterModalImages.forEach(img => {
+            if (img.dataset.src === savedAvatar) {
+                img.classList.add('selected');
+            }
+        });
+    }
+});
+
+let wins = document.getElementById('wins');
+let loses = document.getElementById('loses');
